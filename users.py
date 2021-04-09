cc28573c9d20ab9ce4bed1b296a61152965c2b2a
Joy Kirii <joykirii@gmail.com>
2021-04-09 12:08:23 +0300

    added function to create new user

---
+++ users.py
@@ -0,0 +1,9 @@
+class Users:
+    """
+    Class that generates new instances of contacts.
+    """
+    users_list = []
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
