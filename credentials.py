99b1e3ffb0af22b69c74f11a840470d497d81545
Joy Kirii <joykirii@gmail.com>
2021-04-09 21:12:38 +0300

    add and tested methods to create multiple credentials and delete credentials

--- credentials.py
+++ credentials.py
@@ -9,3 +9,15 @@ class Credentials:
         self.account = account
         self.user_name = user_name
         self.account_password = account_password
+
+    def save_credential(self):
+        """
+        method that saves contact object
+        """
+        Credentials.credential_list.append(self)
+
+    def delete_credential(self):
+        """
+        method deletes a saved credential from credential_list
+        """
+        Credentials.credential_list.remove(self)
--- credentials_test.py
+++ credentials_test.py
@@ -3,5 +3,57 @@ from credentials import Credentials
 
 
 class TestCredentials(unittest.TestCase):
+    """
+    Test class to test credentials class behaiours
+    """
 
-    "Test class to test the creadentials class behaviours"
+    def setUp(self):
+        """
+        Set up method that will run before each test case
+        """
+        self.new_credential = Credentials("Twitter", "Kare", "0")
+
+    def test_init(self):
+        """
+        test if the objects have initialized properly
+        """
+        self.assertEqual(self.new_credential.account, "Twitter")
+        self.assertEqual(self.new_credential.user_name, "Kare")
+        self.assertEqual(self.new_credential.account_password, "0")
+
+    def test_save_credential(self):
+        """
+        Test to see if the contact object is saved into the contact list
+        """
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credentials.credential_list), 1)
+
+    def tearDown(self):
+        """
+        clean up test case after every run
+        """
+        Credentials.credential_list = []
+
+    def test_save_multiple_credentials(self):
+        """
+        test to check if we can save multiple contact
+        """
+        self.new_credential.save_credential()
+        test_credential = Credentials("Instagram", "Temi", "1")
+        test_credential.save_credential()
+        self.assertEqual(len(Credentials.credential_list), 2)
+
+    def test_delete_contact(self):
+        """
+        test to see if we can remove a credential
+        """
+        self.new_credential.save_credential()
+        test_credential = Credentials("Instagram", "Temi", "1")
+        test_credential.save_credential()
+
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credentials.credential_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
