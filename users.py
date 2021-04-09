8fce639eb5138260ffc99c33f67f6a66425b2d65
Joy Kirii <joykirii@gmail.com>
2021-04-09 12:23:21 +0300

    ran test for add user functionality

--- users.py
+++ users.py
@@ -7,3 +7,10 @@ class Users:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        save_user method saves user objects into contact_list
+
+        """
+        Users.users_list.append(self)
--- users_test.py
+++ users_test.py
@@ -7,3 +7,18 @@ class TestUsers(unittest.TestCase):
     Test class that defines test cases for the contact class behaviours.
 
     '''
+
+    def setUp(self):
+        self.new_user = Users("Joy", "0000")
+
+    def test_init(self):
+        self.assertEqual(self.new_user.username, "Joy")
+        self.assertEqual(self.new_user.password, "0000")
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(Users.users_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
